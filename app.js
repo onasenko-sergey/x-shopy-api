c7b08763ef6cb0b286d3f087b8a1aa8800d37da3
onasenko-sergey <onasenko.sergey@gmail.com>
2017-02-01 01:03:47 +0300

    apply es6 code style

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-var express = require('express');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var index = require('./routes/index');
+const index = require('./routes/index');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -16,21 +16,21 @@ app.use('/', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // send error notification
-  res.status(err.status || 500);
-  if (req.app.get('env') !== 'development') return res.end();
-  // provide error info in development only
-  res.json({
-    message: err.message,
-    stack: err.stack.split('\n')
-  });
+    // send error notification
+    res.status(err.status || 500);
+    if (req.app.get('env') !== 'development') return res.end();
+    // provide error info in development only
+    res.json({ error: {
+        message: err.message,
+        stack: err.stack.split('\n')
+    }});
 });
 
 module.exports = app;
