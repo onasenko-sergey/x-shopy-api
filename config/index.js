a8fd079b8c696898945a2a903a220f2dc22a43b1
onasenko-sergey <onasenko.sergey@gmail.com>
2017-02-03 03:07:56 +0300

    prepare application configuration

---
+++ config/index.js
@@ -0,0 +1,15 @@
+// use let instead of const for testing purposes(rewire can't change const variables)
+let development = require('./env/development');
+let production = require('./env/production');
+let test = require('./env/test');
+
+module.exports = function (env) {
+    switch (env) {
+        case 'production':
+            return Object.assign(development, production);
+        case 'test':
+            return Object.assign(development, test);
+        default:
+            return development;
+    }
+};
