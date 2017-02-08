d2d33f0a1ac005a24ee90844d1756ae309cc9b46
onasenko-sergey <onasenko.sergey@gmail.com>
2017-02-08 22:04:07 +0300

    establish database connection

--- config/env/development.js
+++ config/env/development.js
@@ -1,3 +1,11 @@
-module.exports = {
+const mlab = {
+    username: 'shopy',
+    password: process.env.MLAB_PASS,
+    host: 'ds139949.mlab.com',
+    port: '39949',
+    database: 'shopy'
+};
 
+module.exports = {
+    mongoose_connection_string: `mongodb://${mlab.username}:${mlab.password}@${mlab.host}:${mlab.port}/${mlab.database}`
 };
--- config/env/test.js
+++ config/env/test.js
@@ -1,3 +1,9 @@
-module.exports = {
+const mongodb = {
+    host: 'localhost',
+    port: 27017,
+    database: 'shopy'
+};
 
+module.exports = {
+    mongoose_connection_string: `mongodb://${mongodb.host}:${mongodb.port}/${mongodb.database}`
 };
